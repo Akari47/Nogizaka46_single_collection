7f9dfa60197fdb95e69e0ef10b192da0b56030c4
Akari47 <101469414+Akari47@users.noreply.github.com>
2024-02-06 04:17:45 +0900

    add single table, add single pic 1-10, add about

---
+++ script/1.js
@@ -0,0 +1,9 @@
+const leftbar_status = document.getElementById("leftbar")
+
+function leftbar_close(){
+    leftbar_status.style.display = "none"
+}
+
+function leftbar_open(){
+    leftbar_status.style.display = "block"
+}
