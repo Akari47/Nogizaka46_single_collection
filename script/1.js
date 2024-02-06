6446527540cfeef5d4678e9c2b199e31786ddd81
Akari47 <101469414+Akari47@users.noreply.github.com>
2024-02-06 22:22:44 +0900

    remove leftbar, remove js, add table data

--- script/1.js
+++
@@ -1,9 +0,0 @@
-const leftbar_status = document.getElementById("leftbar")
-
-function leftbar_close(){
-    leftbar_status.style.display = "none"
-}
-
-function leftbar_open(){
-    leftbar_status.style.display = "block"
-}
