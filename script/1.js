3a479d575ad59c824220e3782f695473bb84a47b
Akari47 <101469414+Akari47@users.noreply.github.com>
2024-02-08 13:36:51 +0900

    add popup div, popup appear disappear js

--- script/1.js
+++ script/1.js
@@ -1,2 +1,26 @@
 const images = document.querySelectorAll("img")
 images.forEach(image => {image.draggable = false})
+
+const n_single = 34
+for (let i = 1; i < n_single + 1; i++){
+    const frame_id = 'f' + i
+    const frame_div = document.getElementById(frame_id)
+    frame_div.setAttribute('name', 'frame' + i)
+}
+
+const popup_window = document.getElementById("popup")
+const popup_text_div = document.getElementById("popup_text")
+
+function popup_data(single){
+    popup_text_div.innerHTML ='This is the ' + single + ' single'
+}
+
+function popup_appear(single){
+    popup_window.style.display ="inline-block"
+    popup_data(single)
+}
+
+function popup_disappear(){
+    popup_window.style.display ="none"
+    popup_text_div.innerHTML =''
+}
